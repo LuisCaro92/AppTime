570d1d95a03b5e5b4aaa3e1c9ca82982c470e8c8
luis caro <luis.carosalgado1@gmail.com>
2023-09-12 15:55:26 -0300

    card responsives

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,10 +10,10 @@ function Card({ loadingData, showData, now, day }) {
       {showData === true ? (
         <div className="container-md mx-auto items-center">
           <div className="card mb-3 mx-auto bg-dark text-white">
-            <div className="columns-2 m-20 border-r-emerald-600 gap-0">
-              <div className="bg-black">cxvncvn</div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 m-20  gap-0">
+              <div className="bg-emerald-600 rounded-l-md">cxvncvn</div>
 
-              <div className="bg-white text-black"> fgnxcvn</div>
+              <div className="bg-emerald-300 text-black rounded-r-md"> fgnxcvn</div>
             </div>
           </div>
         </div>
