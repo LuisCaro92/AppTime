f155b11c009a39cd0859b13ac901d5b7d3b44c75
luis caro <luis.carosalgado1@gmail.com>
2023-09-12 17:36:58 -0300

    tiempo del dia terminado

--- src/components/Card.js
+++ src/components/Card.js
@@ -22,23 +22,32 @@ function Card({ loadingData, showData, now, day }) {
   return (
     <div className="mt-5">
       {showData === true ? (
-        <div className="container-md mx-auto items-center">
-          <div className="card mb-3 mx-auto bg-dark text-white items-center text-center">
-            <div className="grid grid-cols-1 sm:grid-cols-2 m-20  gap-0">
-              <div className="bg-emerald-600 rounded-l-md">
-                <h2 className="card-title">{now.name}</h2>
-                <h3 className="card-date">{date}</h3>
-                <p className="card-icon flex items-center">
-                  <img className="img-icon" src={iconUrl} />
-                  {now.weather[0].description}
-                </p>
-                <h1 className="card-temp">
-                  {(now.main.temp - 273.15).toFixed(1)}°C
-                </h1>
+        <div className="container-md mx-auto">
+          <div className="card mb-3 mx-auto bg-dark text-white ">
+            <div className="grid grid-cols-1 sm:grid-cols-2 m-20 relative gap-0">
+              <div className="bg-emerald-600 rounded-l-md group :">
                 <img
                   className="object-cover"
                   src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 />
+                <div className="absolute inset-0 from-transparent via-transparent">
+                  <div className="flex">
+                    <h2 className="card-title text-8xl text-start">
+                      {now.name}
+                    </h2>
+                    <h3 className="card-date ml-80 mt-10">{date}</h3>
+                  </div>
+
+                  <p className="card-icon flex ">
+                    <img className="img-icon w-40" src={iconUrl} />
+                  </p>
+                  <p className="card-icon-description">
+                    {now.weather[0].description}
+                  </p>
+                  <h1 className="card-temp">
+                    {(now.main.temp - 273.15).toFixed(1)}°C
+                  </h1>
+                </div>
               </div>
 
               <div className="bg-emerald-300 text-black rounded-r-md">
