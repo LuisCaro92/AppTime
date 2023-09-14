8eb7644fda1b4dd7677c7daee7432d80df41ba56
luis caro <luis.carosalgado1@gmail.com>
2023-09-13 22:11:59 -0300

    ingresando cambios de estilos

--- src/components/Card.js
+++ src/components/Card.js
@@ -25,18 +25,18 @@ function Card({ loadingData, showData, now, day }) {
         <div className="container-md mx-auto">
           <div className="card mb-3 mx-auto bg-dark text-white ">
             <div className="grid grid-cols-1 sm:grid-cols-2 m-20 relative gap-0">
-              <div className="bg-emerald-600 rounded-l-md group :">
+              <div className="bg-emerald-600 rounded-l-md">
                 <img
                   className="object-cover"
                   src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                 />
-                <div className="absolute inset-0 from-transparent via-transparent">
-                  <div className="flex">
-                    <h2 className="card-title text-8xl text-start">
+                <div className="absolute inset-0 from-transparent via-transparent items-center">
+                  
+                    <h2 className="card-title text-8xl items-center">
                       {now.name}
                     </h2>
-                    <h3 className="card-date ml-80 mt-10">{date}</h3>
-                  </div>
+                    <h3 className="card-date">{date}</h3>
+                  
 
                   <p className="card-icon flex ">
                     <img className="img-icon w-40" src={iconUrl} />
